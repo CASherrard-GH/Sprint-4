bb3a1f39d1ccb1a5097bcbd7ab1fc868d4543d7f
CASherrard-GH <62374199+CASherrard-GH@users.noreply.github.com>
2020-07-24 14:26:48 +0200

    Add files via upload

--- Main.py
+++ Main.py
@@ -16,17 +16,62 @@ cluster = MongoClient("mongodb+srv://user_2001:123@cluster0.qnjpq.mongodb.net/Da
 database = cluster["Data_Tracker"]
 collection = database["Items|Ranks"]
 
-post_chips = {"_id": 1, "Simba" : 7, "Lays": 4}
-post_cooldrinks = {"_id": 2, "Coke": 1, "Fanta" : 11}
-post_chocolates = {"_id": 3 ,"Cadbury": 5, "Tex": 6}
-post_pies = {"_id": 4, "Pepper Steak": 3, "Chicken": 4}
-post_fruit = {"_id": 5, "Pear" : 5, "Apple" : 1, "Orange": 2}
-post_cupcakes = {"_id": 6, "Vanilla" : 4, "Chocolate": 2}
-post_veggies = {"_id": 7, "Spinach" : 13, "Cabbage": 12}
+post_chips = {"_id": 1, "Brand": "Simba", "Rank": 7}
+post_cooldrinks = {"_id": 2, "Brand": "Coke", "Rank": 1}
+post_chocolates = {"_id": 3, "Brand": "Cadbury", "Rank": 9}
+post_pies = {"_id": 4, "Brand": "Pepper Steak", "Rank": 3}
+post_fruit = {"_id": 5, "Brand": "Pear", "Rank": 5}
+post_cupcakes = {"_id": 6, "Brand": "Vanilla", "Rank": 4}
+post_veggies = {"_id": 7, "Brand": "Spinach", "Rank": 13}
+post_chips2 = {"_id": 8, "Brand": "Lays", "Rank": 4}
+post_cooldrinks2 = {"_id": 9, "Brand": "Fanta", "Rank": 11}
+post_chocolates2 = {"_id": 10, "Brand": "Tex", "Rank":  6}
+post_pies2 = {"_id": 11, "Brand": "Chicken", "Rank": 4}
+post_fruit2 = {"_id": 12, "Brand": "Apple", "Rank": 1}
+post_fruit3 = {"_id": 13, "Brand": "Orange", "Rank": 2}
+post_cupcakes2 = {"_id": 14, "Brand": "Chocolate", "Rank": 2}
+post_veggies2 = {"_id": 15, "Brand": "Cabbage", "Rank": 12}
 
-#database.collection.find({"_id": {"$gt": 1}}).sort([("_id", 1), ("_id", -1)]) #$gt selects those documents where the value of the field is greater than (i.e. >) the specified value.
-database.collection.find( {"_id": 2 } )
+
+
+collection.insert_many([post_chips, post_chips2, post_cooldrinks, post_cooldrinks2, post_chocolates, post_chocolates2, post_pies, post_pies2, post_fruit, post_fruit2, post_fruit3, post_cupcakes, post_cupcakes2, post_veggies, post_veggies2])
 
 
 
-collection.insert_many([post_chips, post_chocolates, post_cooldrinks, post_cupcakes, post_fruit, post_pies, post_veggies])
+
+query = { "Rank": 13 }
+
+prnt = collection.find(query)
+for i in prnt:
+  print(i)
+
+query = { "Rank": 12 }
+
+prnt = collection.find(query)
+
+for i in prnt:
+  print(i)
+ 
+    
+query = { "Rank": 11 }
+
+prnt = collection.find(query)
+
+for i in prnt:
+  print(i)
+  
+query = { "Rank": 7 }
+
+prnt = collection.find(query)
+
+for i in prnt:
+  print(i)
+  
+
+  
+query = { "Rank": 5 }
+
+prnt = collection.find(query)
+
+for i in prnt:
+  print(i)
--- Secondary.py
+++ Secondary.py
@@ -10,4 +10,29 @@ post_fruit = {"_id": 2, "Catagory": "Fruit", "Type": "Apple", "Rank": 1}
 post_fruit2 =  {"_id": 3, "Catagory": "Fruit", "Type": "Orange", "Rank": 2}
 
 
+  
 collection.insert_many([post_cooldrink, post_fruit, post_fruit2])
+
+mysort = collection.find().sort("_id", -1)
+for x in mysort:
+  print(x)
+  
+dltquery1 = { "_id": 1 }
+
+collection.delete_one(dltquery1)
+
+
+dltquery2 = { "_id": 2 }
+
+collection.delete_one(dltquery2)
+
+
+
+updatequery = {"Rank": 2}
+updatevalues = { "$set": {"Rank": "1"}}
+
+
+collection.update_one(updatequery, updatevalues)
+
+for j in collection.find():
+  print(j)
